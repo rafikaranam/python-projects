abe44611f76a16325ec687aa6716db562eac3d9b
rafikaranam <rafi.gprec@gmail.com>
2022-09-29 22:04:08 -0700

    Add files via upload

---
+++ password-generator.py
@@ -0,0 +1,41 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+alpha = random.choices(letters,k=nr_letters)
+numeric = random.choices(numbers, k=nr_numbers)
+sym = random.choices(symbols, k=nr_symbols)
+word = alpha+numeric+sym
+random.shuffle(word)
+password = ''
+password = password.join(word)
+
+print(password)
+
+# p = ''
+# for char in range(0,nr_letters):
+#   p += random.choice(letters)
+
+# for char in range(0,nr_numbers):
+#   p += random.choice(numbers)
+# for char in range(0, nr_symbols):
+#   p += random.choice(symbols)
+
+# print(p)
+
+
+
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
